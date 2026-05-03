fe1b89802f348b2f6019fb2fa9631e8086b5279a
VITAZGIO <vitazgio@gmail.com>
2026-05-04 01:32:50 +0300

    BAZA

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Сайт работает 🚀.S"
+    return "Сайт работает 🚀"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
